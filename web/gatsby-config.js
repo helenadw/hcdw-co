968f6ab3f6d0d023539afb77645fcce64c05c153
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2020-06-19 18:24:36 +0100

    removed sass package

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -6,7 +6,7 @@ const {
 module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
-    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-react-helmet', 
     {
       resolve: 'gatsby-source-sanity',
       options: {
