29833af215f857083835f6bafa3c2f4dd6c38abe
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2020-11-22 14:33:13 +0000

    site updates

--- web/src/components/project-preview.js
+++ web/src/components/project-preview.js
@@ -1,3 +1,4 @@
+import { max } from 'date-fns'
 import { Link } from 'gatsby'
 import React from 'react'
 import { cn, buildImageObj } from '../lib/helpers'
@@ -15,8 +16,7 @@ function ProjectPreview (props) {
         {props.mainImage && props.mainImage.asset && (
           <img
             src={imageUrlFor(buildImageObj(props.mainImage))
-              .width(600)
-              .height(Math.floor((9 / 16) * 600))
+              
               .url()}
             alt={props.mainImage.alt}
           />
