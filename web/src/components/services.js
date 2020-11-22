37edf912b65a360b8a83346b8a210e563288a64e
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2020-11-22 20:43:04 +0000

    updates

--- web/src/components/services.js
+++ web/src/components/services.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import servicesStyles from './services.module.css'
-import Code from './icons/code.svg'
-import Drop from './icons/drop.svg'
-import Cube from './icons/cube.svg'
+// import Code from './icons/code.svg'
+// import Drop from './icons/drop.svg'
+// import Cube from './icons/cube.svg'
 
 
 
@@ -14,17 +14,17 @@ function Services() {
                 <h2 className={servicesStyles.servicesTitle}>services</h2>
                 <div className={servicesStyles.servicesCards}>
                     <div className={servicesStyles.servicesCard}>
-                    <img src={Drop} alt='dropper icon'></img>
+                    {/* <img src={Drop} alt='dropper icon'></img> */}
                     <h4 className={servicesStyles.serviceType}>web design</h4>
                     <p>user-friendly design your customers will love</p>
                     </div>
                     <div className={servicesStyles.servicesCard}>
-                    <img src={Code} alt='code icon'></img>
+                    {/* <img src={Code} alt='code icon'></img> */}
                     <h4 className={servicesStyles.serviceType}>web development</h4>
                     <p>lightweight solutions that are easy to maintain</p>
                     </div>
                     <div className={servicesStyles.servicesCard}>
-                    <img src={Cube} alt='cube icon'></img>
+                    {/* <img src={Cube} alt='cube icon'></img> */}
                     <h4 className={servicesStyles.serviceType}>automation</h4>
                     <p>systems that save time in your business</p>
                     </div>
