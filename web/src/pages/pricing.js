8d555410ed41a9f2ba1c9caff6ed867cda59c01b
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2021-02-17 14:50:35 +0000

    updated

--- web/src/pages/pricing.js
+++ web/src/pages/pricing.js
@@ -1,56 +1,56 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Container from '../components/container'
-import GraphQLErrorList from '../components/graphql-error-list'
-import ServicePreviewGrid from '../components/service-preview-grid'
-import SEO from '../components/seo'
-import Layout from '../containers/layout'
-import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from '../lib/helpers'
-import { responsiveTitle1 } from '../components/typography.module.css'
+// import React from 'react'
+// import { graphql } from 'gatsby'
+// import Container from '../components/container'
+// import GraphQLErrorList from '../components/graphql-error-list'
+// import ServicePreviewGrid from '../components/service-preview-grid'
+// import SEO from '../components/seo'
+// import Layout from '../containers/layout'
+// import { mapEdgesToNodes, filterOutDocsWithoutSlugs } from '../lib/helpers'
+// import { responsiveTitle1 } from '../components/typography.module.css'
 
-export const query = graphql`
-  query PricingPageQuery {
-    service: allSanityPricing(limit: 12, sort: { fields: [publishedAt], order: DESC }) {
-      edges {
-        node {
-          id
-          publishedAt
-          mainImage {
-            asset {
-              _key
-            }
-            alt 
-          }
-          title
-          slug {
-            current
-          }
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query PricingPageQuery {
+//     service: allSanityPricing(limit: 12, sort: { fields: [publishedAt], order: DESC }) {
+//       edges {
+//         node {
+//           id
+//           publishedAt
+//           mainImage {
+//             asset {
+//               _key
+//             }
+//             alt 
+//           }
+//           title
+//           slug {
+//             current
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
 
-const PricingPage = props => {
-    const { data, errors } = props
-    if (errors) {
-        return (
-        <Layout>
-            <GraphQLErrorList errors={errors} />
-        </Layout>
-        )
-    }
-    const pricingNodes =
-        data && data.pricing && mapEdgesToNodes(data.pricing).filter(filterOutDocsWithoutSlugs)
-    return(
-        <Layout>
-            <SEO title='Pricing' />
-            <Container>
-            <h1 className={responsiveTitle1}>Pricing</h1>
-            {pricingNodes && pricingNodes.length > 0 && <PricingPreviewGrid nodes={pricingNodes} />}
-            <PricingPreviewGrid/>
-            </Container>
-        </Layout>
-    )
-} 
-export default PricingPage
+// const PricingPage = props => {
+//     const { data, errors } = props
+//     if (errors) {
+//         return (
+//         <Layout>
+//             <GraphQLErrorList errors={errors} />
+//         </Layout>
+//         )
+//     }
+//     const pricingNodes =
+//         data && data.pricing && mapEdgesToNodes(data.pricing).filter(filterOutDocsWithoutSlugs)
+//     return(
+//         <Layout>
+//             <SEO title='Pricing' />
+//             <Container>
+//             <h1 className={responsiveTitle1}>Pricing</h1>
+//             {pricingNodes && pricingNodes.length > 0 && <PricingPreviewGrid nodes={pricingNodes} />}
+//             <PricingPreviewGrid/>
+//             </Container>
+//         </Layout>
+//     )
+// } 
+// export default PricingPage
